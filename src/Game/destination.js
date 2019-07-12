646c7ece97e6ef7eb54f8865e7e7f1d924192376
bboyrajib <bboyrajib@gmail.com>
2019-07-12 08:29:02 +0530

    Planets API fixed

--- src/Game/destination.js
+++ src/Game/destination.js
@@ -3,13 +3,54 @@ import './destination.css';
 
 class Destination extends Component {
     state = { 
-        planets:[]
+        remPlanets:[],
+        planets:[],
+        planetOne:"--your choice--",
+        planetTwo:"--your choice--",
+        planetThree:"--your choice--",
+        planetFour:"--your choice--"
      }
-
-    componentDidMount(){
-        
+    selPlanets = {
+        planetOne:false,
+        planetTwo:false,
+        planetThree:false,
+        planetFour:false
     }
+    componentDidMount(){
+        fetch("https://findfalcone.herokuapp.com/planets")
+            .then(res => res.json())
+            .then((remPlanets) => {
+                this.setState({remPlanets,planets:remPlanets});
+            });
+    }
+    handleChange = (e) => {
+        
+        if(this.selPlanets[e.target.name]===false){
 
+            this.setState({[e.target.name]:e.target.value});
+            this.selPlanets[e.target.name]=true;
+            var remPlanet = this.state.remPlanets.filter(planet => {
+                return planet.name!==e.target.value ;
+             });
+             
+            this.setState({remPlanets:remPlanet});
+        }else{
+            
+            var prevPlanet = this.state[e.target.name];
+            this.setState({[e.target.name]:e.target.value});
+            var planetObj = this.state.planets.filter(planet => {
+                return planet.name===prevPlanet ;
+             });
+            let _remPlanets= [...this.state.remPlanets]
+            _remPlanets.push(planetObj[0]);
+            var remPlanets = _remPlanets.filter(planet => {
+                 return planet.name!==e.target.value ;
+            });
+            this.setState({remPlanets});
+        }
+       
+
+    }
     render() { 
         return ( 
             <div className="container h-100 d-flex flex-column">
@@ -18,27 +59,40 @@ class Destination extends Component {
                 <div className="row text-center">
                     
                     <div className="col"><h3>Planet One</h3>
-                            <select className="m-3">
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
+                            <select onChange={this.handleChange} className="m-3" name="planetOne" value={this.state.planetOne}>
+                                <option>{this.state.planetOne}</option>
+                                {this.state.remPlanets.map(planet => <option key={planet.name} >{planet.name}</option>)}
                             </select>
                             <br/>
-                            <input type="radio" name="vehicleOne" value="1" className="m-2"/><label>One</label><br/>
-                            <input type="radio" name="vehicleOne" value="2" className="m-2"/><label>One</label><br/>
-                            <input type="radio" name="vehicleOne" value="3" className="m-2"/><label>One</label><br/>
-                            <input type="radio" name="vehicleOne" value="4" className="m-2"/><label>One</label>
+                            
                     </div>
                     
                     <div className="col"><h3>Planet Two</h3>
-                    
+                            <select onChange={this.handleChange} className="m-3" name="planetTwo" value={this.state.planetTwo}>
+                                <option>{this.state.planetTwo}</option>
+                                {this.state.remPlanets.map(planet => <option key={planet.name} >{planet.name}</option>)}
+                            </select>
+                            <br/>
                     
                     </div>
 
-                    <div className="col"><h3>Planet Three</h3></div>
+                    <div className="col"><h3>Planet Three</h3>
+                            <select onChange={this.handleChange} className="m-3" name="planetThree" value={this.state.planetThree}>
+                                <option>{this.state.planetThree}</option>
+                                {this.state.remPlanets.map(planet => <option key={planet.name} >{planet.name}</option>)}
+                            </select>
+                            <br/>
+    
+                    </div>
 
-                    <div className="col"><h3>Planet Four</h3></div><br/><br/>
+                    <div className="col"><h3>Planet Four</h3>
+                            <select onChange={this.handleChange} className="m-3" name="planetFour" value={this.state.planetFour}>
+                                <option>{this.state.planetFour}</option>
+                                {this.state.remPlanets.map(planet => <option key={planet.name} >{planet.name}</option>)}
+                            </select>
+                            <br/>
+                    
+                    </div><br/><br/>
                     
                 </div>
                 <div className="row justify-content-center">
