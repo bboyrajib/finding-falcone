608891d73cbc48e8e67512333797f7cd74f2cec2
bboyrajib <bboyrajib@gmail.com>
2019-07-12 22:20:54 +0530

    Updates

---
+++ precache-manifest.c91b8b8d02278ce7814dc9b4d238ead4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9ed6f908b3da20b09c5927cb96f986",
+    "url": "/finding-falcone/index.html"
+  },
+  {
+    "revision": "c1ac358d08750c6590f8",
+    "url": "/finding-falcone/static/css/main.d92edaee.chunk.css"
+  },
+  {
+    "revision": "5ba6af502d246dffc75b",
+    "url": "/finding-falcone/static/js/2.b16d4712.chunk.js"
+  },
+  {
+    "revision": "c1ac358d08750c6590f8",
+    "url": "/finding-falcone/static/js/main.e75611f1.chunk.js"
+  },
+  {
+    "revision": "69b0e01df1478d287b81",
+    "url": "/finding-falcone/static/js/runtime~main.ed3899b0.js"
+  }
+]);
